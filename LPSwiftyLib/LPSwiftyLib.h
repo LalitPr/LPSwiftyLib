49d51350b82d327eb78fd2186bca601dea16c4df
Lalit Pratap <lalitp@dewsolutions.in>
2022-09-07 12:11:34 +0530

    Initial Commit

---
+++ LPSwiftyLib/LPSwiftyLib.h
@@ -0,0 +1,18 @@
+//
+//  LPSwiftyLib.h
+//  LPSwiftyLib
+//
+//  Created by lalit Pratap on 07/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LPSwiftyLib.
+FOUNDATION_EXPORT double LPSwiftyLibVersionNumber;
+
+//! Project version string for LPSwiftyLib.
+FOUNDATION_EXPORT const unsigned char LPSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPSwiftyLib/PublicHeader.h>
+
+
