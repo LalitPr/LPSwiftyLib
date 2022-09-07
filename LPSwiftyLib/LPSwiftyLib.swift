bc6ee032c455fc51e51c94701c78a1f2d37b3f49
Lalit Pratap <lalitp@dewsolutions.in>
2022-09-07 14:53:33 +0530

    Added test cases

---
+++ LPSwiftyLib/LPSwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  LPSwiftyLib.swift
+//  LPSwiftyLib
+//
+//  Created by lalit Pratap on 07/09/22.
+//
+
+import Foundation
+
+public final class LPSwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- LPSwiftyLibTests/LPSwiftyLibTests.swift
+++ LPSwiftyLibTests/LPSwiftyLibTests.swift
@@ -9,6 +9,20 @@ import XCTest
 @testable import LPSwiftyLib
 
 class LPSwiftyLibTests: XCTestCase {
+    
+    var swiftyLib: LPSwiftyLib!
+
+    override func setUp() {
+        swiftyLib = LPSwiftyLib()
+    }
+
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
